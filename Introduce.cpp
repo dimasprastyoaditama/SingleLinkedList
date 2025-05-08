090e56aab1f881f7eb27cc45be7135219e9318f8
dimasprastyoaditama <prastyod647@gmail.com>
2025-05-08 15:19:01 +0700

    commit3

--- Introduce.cpp
+++ Introduce.cpp
@@ -30,6 +30,11 @@ public:
 
     if (START == NULL || nim <= START->noMhs)
    {
-    
+     
+      if ((START != NULL ) && (nim == START->noMhs))
+      {
+        cout << "\nDuplikasi noMhs tidak diijinkan\n";
+        return;
+      }
    }
 }
